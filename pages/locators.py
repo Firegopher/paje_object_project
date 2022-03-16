494c6e51cc8b047aed214a8bbfb0d1ceefb30f38
Баранов Александр Сергеевич <asbaranov@phoenixit.ru>
2022-03-16 19:20:44 +0300

    Add tests for promo

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,7 @@ class LoginPageLocators():
 
 class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    PRODUCT_NAME_IN_MESSAGE = (By.CSS_SELECTOR, "#messages .alert:nth-child(1) .alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
--- pages/product_page.py
+++ pages/product_page.py
@@ -4,5 +4,14 @@ from .locators import ProductPageLocators
 class ProductPage(BasePage):
     def press_add_to_basket_button(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+        assert True, "Button not found"
     
+    def should_be_message_product_has_been_added_to_basket(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_in_message = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE).text
+        assert product_name_in_message == product_name, "Product name does not match with product name in basket"
 
+    def should_be_message_with_basket_total(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE)
+        basket_total = self.browser.find_element(ProductPageLocators.BASKET_TOTAL)
+        assert basket_total == product_price, "Basket total does not match with product price"
